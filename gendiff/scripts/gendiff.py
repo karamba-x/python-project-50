4f799f2d8face18e68e9ea1822aa9b1289635cba
karamba <kadykov.x@gmail.com>
2025-04-15 14:15:24 +0900

    step 4

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,17 @@
+import argparse
+from gendiff.scripts.generate_diff import generate_diff
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file', help='path to first file')
+    parser.add_argument('second_file', help='path to second file')
+
+    parser.add_argument('-f', '--format', help='set format of output')
+
+    args = parser.parse_args()
+    print(generate_diff(args.first_file, args.second_file))
+
+if __name__ == '__main__':
+    main()
---
+++ gendiff/scripts/generate_diff.py
@@ -0,0 +1,23 @@
+import json
+
+def generate_diff(file1_path, file2_path):
+    with open(file1_path) as f1, open(file2_path) as f2:
+        data1 = json.load(f1)
+        data2 = json.load(f2)
+
+    keys = sorted(set(data1.keys()) | set(data2.keys()))
+    result = ['{']
+
+    for key in keys:
+        if key in data1 and key not in data2:
+            result.append(f"  - {key}: {data1[key]}")
+        elif key not in data1 and key in data2:
+            result.append(f"  + {key}: {data2[key]}")
+        elif data1[key] != data2[key]:
+            result.append(f"  - {key}: {data1[key]}")
+            result.append(f"  + {key}: {data2[key]}")
+        else:
+            result.append(f"    {key}: {data1[key]}")
+
+    result.append('}')
+    return '\n'.join(result)
